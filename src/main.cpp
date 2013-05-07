2886107ec2171d6168a10b03ae85cf410d8e69f7
Ben Ruijl <benruyl@gmail.com>
2013-05-07 15:29:46 +0200

    Fixed error

--- src/main.cpp
+++ src/main.cpp
@@ -130,7 +130,7 @@ int main(int argc, char** argv) {
     
     if (config.ratioFreq > 0) {
         TriangleRatioObservable* triangleRatioObservable = new
-                TriangleRatioObservable(config.accFreq, &simulation);
+                TriangleRatioObservable(config.ratioFreq, &simulation);
         simulation.addObservable(triangleRatioObservable);
     }
 
